ed1e134534e160cf1776f31b60334b07654e51ed
AnvarSh1 <anvar.shirinbayli@gmail.com>
2018-04-30 12:48:35 +0400

    Update ochkbot.py

--- ochkbot.py
+++ ochkbot.py
@@ -11,36 +11,43 @@ def send_welcome(message):
 	bot.send_message(message.chat.id, "Дратути! \nЭтот бот создан для обратной связи с каналом Очко Баяна - https://t.me/ochkobayanachannel \nВы таки хочете нам шо-то сказать? \nПошлите нам что угодно - картинку, видосик, ссылочку, стикер - даже просто пожелания, и возможно мы их опубликуем!\n*ноэтонеточно*")
 
 	
-###handler for affirmative response
-
 yopta = ['ДА', 'ДАА', 'ДААА', 'YES', 'АГА', 'ЫЫЫ', 'Ы', 'DA', 'DAA', 'DAAA']
 
-@bot.message_handler(func=lambda m: True)
-def affirm_resp (message):
-        if message.text.upper() in yopta:
-                bot.reply_to(message, 'Спасибо, ёпта :3')
+
 
 
 ###simple about_us info
 @bot.message_handler(commands=['about', 'wassup'])
-def send_welcome(message):
-	bot.send_message(message.chat.id, "Команда упоротых адептов Мемологии, поклоняющихся Баяну, Паше Дурову и святому духу Прокрастинаци и желающих провернуть фсосайети по-азербайджански. С нами вкусно, кислотно и трешово, будь на связи! \n \nБот написан и протестирован лично командой канала\nКод канала доступен по ссылке: https://github.com/AnvarSh1/ContactUs-SendStuff-Telegram-Bot/")
+def send_info (message):
+        bot.send_message(message.chat.id, "Команда упоротых адептов Мемологии, поклоняющихся Баяну, Паше Дурову и святому духу Прокрас$
 
 
 
 ###handler for everything except stickers - forwards message to admins
 @bot.message_handler(content_types=['photo', 'document', 'audio', 'voice', 'text', 'video_note', 'location'])
-def echo_all(message):
-	bot.send_message(message.chat.id, "Мы получили ваше сообщение, спасибо!")
-	bot.forward_message(admin1_chat_id, message.chat.id, message.message_id)###Admin1
- 	bot.forward_message(admin2_chat_id, message.chat.id, message.message_id)###Admin2
-	bot.forward_message(admin3_chat_id, message.chat.id, message.message_id)###Admin3
-	bot.forward_message(admin4_chat_id, message.chat.id, message.message_id)###Admin4
-	bot.forward_message(admin5_chat_id, message.chat.id, message.message_id)###Admin5
-	bot.forward_message(admin6_chat_id, message.chat.id, message.message_id)###Admin6
-	bot.forward_message(admin7_chat_id, message.chat.id, message.message_id)###Admin7
-	bot.forward_message(admin8_chat_id, message.chat.id, message.message_id)###Admin8
-	bot.forward_message(admin9_chat_id, message.chat.id, message.message_id)###Admin9
+def echo_pic(message):
+        if message.text.upper() in yopta:
+                bot.send_message(message.chat.id, 'Спасибо ёпта :3')
+		bot.forward_message(admin1_chat_id, message.chat.id, message.message_id)###Admin1
+ 		bot.forward_message(admin2_chat_id, message.chat.id, message.message_id)###Admin2
+		bot.forward_message(admin3_chat_id, message.chat.id, message.message_id)###Admin3
+		bot.forward_message(admin4_chat_id, message.chat.id, message.message_id)###Admin4
+		bot.forward_message(admin5_chat_id, message.chat.id, message.message_id)###Admin5
+		bot.forward_message(admin6_chat_id, message.chat.id, message.message_id)###Admin6
+		bot.forward_message(admin7_chat_id, message.chat.id, message.message_id)###Admin7
+		bot.forward_message(admin8_chat_id, message.chat.id, message.message_id)###Admin8
+		bot.forward_message(admin9_chat_id, message.chat.id, message.message_id)###Admin9
+        else:
+             	bot.send_message(message.chat.id, "Мы получили ваше сообщение, спасибо! \nЕсли вы хотите чтоб мы упомянули вас - ответ$
+		bot.forward_message(admin1_chat_id, message.chat.id, message.message_id)###Admin1
+ 		bot.forward_message(admin2_chat_id, message.chat.id, message.message_id)###Admin2
+		bot.forward_message(admin3_chat_id, message.chat.id, message.message_id)###Admin3
+		bot.forward_message(admin4_chat_id, message.chat.id, message.message_id)###Admin4
+		bot.forward_message(admin5_chat_id, message.chat.id, message.message_id)###Admin5
+		bot.forward_message(admin6_chat_id, message.chat.id, message.message_id)###Admin6
+		bot.forward_message(admin7_chat_id, message.chat.id, message.message_id)###Admin7
+		bot.forward_message(admin8_chat_id, message.chat.id, message.message_id)###Admin8
+		bot.forward_message(admin9_chat_id, message.chat.id, message.message_id)###Admin9
   
   
   
